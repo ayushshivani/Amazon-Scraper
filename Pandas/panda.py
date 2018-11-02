b65a7a82ea7c2b58955f78ad040309da59ff2436
Ayush Shivani <ayushshivani12345@gmail.com>
2018-11-02 22:26:37 +0530

    CleanUp

--- Pandas/panda.py
+++ Pandas/panda.py
@@ -6,9 +6,9 @@ movies = pd.read_excel(excel_file)
 
 for i in range(1):
 	movies1 = pd.read_excel(excel_file,sheetname = i)
-	# print(movies1.head())
+	writer = pd.ExcelWriter('movies.xlsx',engine = 'xlsxwriter')
 	for index,row in movies1.iterrows():
-		print(row[0])
+		
 
 
 
