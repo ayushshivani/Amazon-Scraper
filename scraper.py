c6bfa29692b468fd50a38056e95907a27d7e19d0
Ayush Shivani <ayushshivani12345@gmail.com>
2018-11-03 03:16:51 +0530

    Wip- wrote into xlsx file

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 from lxml import html
 import csv
 import time
+import datetime
 import sys
 import threading
 import requests
@@ -45,122 +46,137 @@ def AmzonParser(url):
                 choice(proxy))}, timeout=3)
     plain_text = page.text
     soup = BeautifulSoup(plain_text, "lxml")
-    print(soup)
+    # print(soup)
     result_product = soup.findAll('div', {'id': 'atfResults'})
-    print(result_product)
+    # print(result_product)
     if len(result_product) is not 0:
-	    a_text = result_product[0]
-	    result_a = a_text.findAll('a', {'class': 'a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal'})
-	    if result_a is not None:
-		    result_url = result_a[0].get('href')
-		    # if result_url is not None:
+        a_text = result_product[0]
+        result_a = a_text.findAll(
+            'a', {
+                'class': 'a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal'})
+        if result_a is not None:
+            result_url = result_a[0].get('href')
+            # if result_url is not None:
 
-		    # print(choice(proxy))
-		    PROXY = choice(proxy)
-		    chrome_options = webdriver.ChromeOptions()
-		    chrome_options.add_argument('--proxy-server=%s' % PROXY)
+            # print(choice(proxy))
+            PROXY = choice(proxy)
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
-		    chrome = webdriver.Chrome(executable_path='./chromedriver')
-		    chrome.get(result_url)
-		    chrome.find_element_by_id("nav-global-location-slot").click()
-		    time.sleep(2)
-		    zipElement = chrome.find_element_by_id("GLUXZipUpdateInput")
-		    zipElement.send_keys('32818')
-		    time.sleep(1)
-		    zipElement.send_keys(Keys.ENTER)
-		    time.sleep(1)
-		    chrome.find_element_by_name('glowDoneButton').click()
-		    time.sleep(1)
-		    # print(chrome.page_source)
-		    detail_soup = BeautifulSoup(chrome.page_source, "lxml")
-		    chrome.quit()
+            chrome = webdriver.Chrome(executable_path='./chromedriver')
+            chrome.get(result_url)
+            chrome.find_element_by_id("nav-global-location-slot").click()
+            time.sleep(2)
+            zipElement = chrome.find_element_by_id("GLUXZipUpdateInput")
+            zipElement.send_keys('32818')
+            time.sleep(1)
+            zipElement.send_keys(Keys.ENTER)
+            time.sleep(1)
+            chrome.find_element_by_name('glowDoneButton').click()
+            time.sleep(1)
+            # print(chrome.page_source)
+            detail_soup = BeautifulSoup(chrome.page_source, "lxml")
+            chrome.quit()
 
-		    # print(detail_soup)
-		    product_price = detail_soup.findAll(
-		        'span', {'id': 'price_inside_buybox'})
+            # print(detail_soup)
+            product_price = detail_soup.findAll(
+                'span', {'id': 'price_inside_buybox'})
 
-		    if len(product_price) == 0:
-		        product.append("NULL")
-		    else:
-		        price = product_price[0].text
-		        product.append(price)
-		    # print(price)
+            # print(price)
 
-		    # prime_details = detail_soup.findAll(
-		    #     'span', {'id': 'price-shipping-message'})
-		    # print(prime_details)
-		    # is_Prime = prime_details[0].find('i', {'class': 'a-icon
-		    # a-icon-prime'})
-		    product.append(0)
-		    product.append(0)
+            prime_details = detail_soup.findAll('a', {'id': 'SSOFpopoverLink'})
+            if len(prime_details) != 0:
+                if len(product_price) == 0:
+                    product.append("NULL")
+                else:
+                    price = product_price[0].text.strip()
+                    product.append(price)
+                product.append(0)
+                product.append(0)
+            else:
+                product.append(0)
+                if len(product_price) == 0:
+                    product.append("NULL")
+                else:
+                    price = product_price[0].text.strip()
+                    product.append(price)
+                product_shipping = detail_soup.find(
+                    'div', {'id': 'desktop_qualifiedBuyBox'})
+                if len(product_shipping) != 0:
+                    product_shipping = product_shipping.find(
+                        'span', {'class': 'a-size-base a-color-secondary'})
+                    if len(product_shipping) != 0:
+                        product_shipping = product_shipping.text.strip()
+                        product.append(product_shipping)
 
-		    nof_sellers = detail_soup.find('span', {'id': 'mbc-upd-olp-link'})
-		    if nof_sellers is not None:
-		        product_nof_sellers = nof_sellers.text.strip()
-		        product_nof_sellers = product_nof_sellers.split()
-		        # print(product_nof_sellers[1])
-		        product_nof_sellers = product_nof_sellers[1].replace(
-		            '(', '').replace(')', '')
-		        product.append(product_nof_sellers)
-		    else:
-		        product.append("NULL")
-		    # print(product_nof_sellers)
+            nof_sellers = detail_soup.find('span', {'id': 'mbc-upd-olp-link'})
+            if nof_sellers is not None:
+                product_nof_sellers = nof_sellers.text.strip()
+                product_nof_sellers = product_nof_sellers.split()
+                # print(product_nof_sellers[1])
+                product_nof_sellers = product_nof_sellers[1].replace(
+                    '(', '').replace(')', '')
+                product.append(product_nof_sellers)
+            else:
+                product.append("NULL")
+            # print(product_nof_sellers)
 
-		    product_table = detail_soup.findAll(
-		        'table', {'id': 'productDetails_detailBullets_sections1'})
-		    # print(product_table)
-		    if product_table is not None:
-		        rows = product_table[0].findAll('tr')
-		        # print(rows)
-		        for row in rows:
-		            col = row.find('th')
-		            if col is not None:
-		                col_name = col.text.strip()
-		                # print(col_name.strip())
-		                if col_name == 'Shipping Weight':
-		                    product_weight = row.find('td')
-		                    if product_weight is not None:
-		                        product_weight = product_weight.text.strip()
-		                    else:
-		                        product_weight = "Null"
+            product_table = detail_soup.findAll(
+                'table', {'id': 'productDetails_detailBullets_sections1'})
+            # print(product_table)
+            if product_table is not None:
+                rows = product_table[0].findAll('tr')
+                # print(rows)
+                for row in rows:
+                    col = row.find('th')
+                    if col is not None:
+                        col_name = col.text.strip()
+                        # print(col_name.strip())
+                        if col_name == 'Shipping Weight':
+                            product_weight = row.find('td')
+                            if product_weight is not None:
+                                product_weight = product_weight.text.strip()
+                            else:
+                                product_weight = "Null"
 
-		                if col_name == 'Best Sellers Rank':
-		                    product_BSR = row.find('td')
-		                    # print(product_BSR)
-		                    if product_BSR is not None:
-		                        product_BSR_span = product_BSR.findAll('span')
-		                        if product_BSR is not None:
-		                            for i in range(1):
-		                                for word in product_BSR_span[i].text.split():
-		                                    # print(word)
-		                                    if word[0] == '#':
-		                                        rank.append(word)
-		                                    elif word == 'in':
-		                                        pass
-		                                    else:
-		                                        BSR_name.append(word)
-		                else:
-		                    product_weight = "Null"
+                        if col_name == 'Best Sellers Rank':
+                            product_BSR = row.find('td')
+                            # print(product_BSR)
+                            if product_BSR is not None:
+                                product_BSR_span = product_BSR.findAll('span')
+                                if product_BSR is not None:
+                                    for i in range(1):
+                                        for word in product_BSR_span[i].text.split(
+                                        ):
+                                            # print(word)
+                                            if word[0] == '#':
+                                                rank.append(word)
+                                            elif word == 'in':
+                                                pass
+                                            else:
+                                                BSR_name.append(word)
+                        else:
+                            product_weight = "Null"
 
-		    # print(rank)
-		    # print(BSR_name)
-		    product.append(rank[0])
-		    product.append(BSR_name[0])  # name of BSR
-		    product.append(rank[1])
-		    product.append(BSR_name[1])  # name of BSR
+            # print(rank)
+            # print(BSR_name)
+            product.append(rank[0])
+            product.append(BSR_name[0])  # name of BSR
+            product.append(rank[1])
+            product.append(BSR_name[1])  # name of BSR
 
-		    seller = detail_soup.find('div', {'id': 'merchant-info'})
-		    if seller is not None:
-		        seller_info = seller.find('a')
-		        if seller_info is not None:
-		            seller_info = seller_info.text.strip()
-		        else:
-		            seller_info = "Null"
-		    else:
-		        seller_info = "Null"
-		    # print(seller_info)
-		    product.append(product_weight)
-		    product.append(seller_info)
+            seller = detail_soup.find('div', {'id': 'merchant-info'})
+            if seller is not None:
+                seller_info = seller.find('a')
+                if seller_info is not None:
+                    seller_info = seller_info.text.strip()
+                else:
+                    seller_info = "Null"
+            else:
+                seller_info = "Null"
+            # print(seller_info)
+            product.append(product_weight)
+            product.append(seller_info)
 
     return product
 
@@ -190,33 +206,42 @@ def main():
     excel_file = 'Input.xlsx'
 
     """Testing"""
-    product_id = "609456647823"
-    product_url = url_part1 + str(product_id) + url_part2
-    #     	# print(product_url)
-    AmzonParser(product_url)
+    # product_id = "074427857097"
+    # product_url = url_part1 + str(product_id) + url_part2
+    # #     	# print(product_url)
+    # AmzonParser(product_url)
 
-    # data = xlrd.open_workbook('Input.xlsx')
-    # for sheetno in range(3):
-    #     sheet = data.sheet_by_index(sheetno)
-    #     worksheet = workbook.add_sheet('Mysheet')
-    #     # print(sheet.nrows)
-    #     for i in range(1, sheet.nrows):
-    #         product = []
-    #         product_id = sheet.cell(i, 0).value
+    data = xlrd.open_workbook('Input.xlsx')
+    for sheetno in range(3):
+        sheet = data.sheet_by_index(sheetno)
+        worksheet = workbook.add_sheet('Mysheet')
+        # print(sheet.nrows)
+        for i in range(1, sheet.nrows):
+            product = []
+            product_id = sheet.cell(i, 0).value
 
-    #         if isinstance(product_id, float):
-    #             product_id = str(int(product_id))
-    #         # print((product_id))
+            if isinstance(product_id, float):
+                product_id = str(int(product_id))
+            # print((product_id))
 
-    #         while(len(product_id) < 13):
-    #             product_id = '0' + product_id
+            while(len(product_id) < 13):
+                product_id = '0' + product_id
 
-    #         product_url = url_part1 + product_id + url_part2
-    #         # print(product_url)
-    #         product = AmzonParser(product_url)
-    #         print(product,productDetails_detailBullets_sections1)
+            product_url = url_part1 + product_id + url_part2
+            # print(product_url)
+            product = AmzonParser(product_url)
+            print(product, product_id)
+            date = datetime.datetime.now()
+            current_date = str(date.month) + "/" + str(date.day) + "/" + str(date.year - 2000)
+            
+            worksheet.write(i, 0, current_date)
+            worksheet.write(i, 1, product_id)
 
-    #     break
+            for j in range(2, 12):
+                worksheet.write(i, j, product[j - 2])
+            break
+        break
+    
     workbook.save("Output.xlsx")
 
 
